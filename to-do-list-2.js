0a5bc644f59aa6d6c64e8cdc8ad0870cf67cbfb0
Alexei <alexei@Sherab-Zangpos-MacBook-Pro.local>
2020-10-06 14:40:28 +0200

    merging done and delete button features to Master

--- to-do-list-2.js
+++ to-do-list-2.js
@@ -1,13 +1,46 @@
-/* check user input from form. If valid, create new list item from it */
+
+/* Check user input from form. If valid, create new list item from it */
 function submitItem() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("newItem").value;
-    var t = document.createTextNode(inputValue);
+    const li = document.createElement("li");
+    const inputValue = document.getElementById("text-box").value;
+    const t = document.createTextNode(inputValue);
     li.appendChild(t);
+
     if (inputValue === '') {
         alert("You need to write a task!");
     } else {
         document.getElementById("to-do-list").appendChild(li);
+        addDeleteButton(li);
+        addDoneButton(li);
     }
-    document.getElementById("newItem").value = '';
+    /* clearing textbox after submitting input */
+    document.getElementById("text-box").value = '';
     }
+
+function clearList() {
+    document.getElementById("to-do-list").innerHTML = '';
+}
+
+function addDeleteButton(li) {
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "x";
+    deleteButton.setAttribute("onclick", "deleteItem(this)");
+    li.appendChild(deleteButton);
+} 
+
+function addDoneButton(li) {
+    const doneButton = document.createElement("button");
+    doneButton.innerHTML = "Done";
+    doneButton.setAttribute("onclick", "doneItem(this)");
+    li.appendChild(doneButton);
+} 
+
+function deleteItem(btn) {
+    btn.parentNode.remove();
+}
+
+function doneItem(btn) {
+    btn.parentNode.style = "text-decoration:line-through; list-style-type:none";
+    btn.innerHTML
+}
+
