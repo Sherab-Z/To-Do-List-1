0f072e5ac6c677962b8b50d269012b7ca6d611b3
Sherab Weig <sherab@wholetreedev.com>
2023-12-23 00:30:55 +0100

    refactor js code to use template, but tasks not created

--- script/to-do-list-2.js
+++ script/to-do-list-2.js
@@ -1,47 +1,47 @@
 
 /* Create a new task item from the text-box input, append buttons to the task item, add that to the list, and clear the text-box */
 function addTask() {
-    const task = document.createElement("li");
-    task.classList.add("task", "box", "responsive-task"); // Add the "task" class to the new task el
-
-    const textInput = document.getElementById("text-box").value;
+    const template = document.getElementById('task-template');
+    const taskClone = template.contentEditable.cloneNode(true);
+    const task = taskClone.querySelector('.task');
+    const textElement = taskClone.querySelector('.text');
+    
+    const textInput = document.getElementById('text-box').value;
     const t = document.createTextNode(textInput);
-    task.appendChild(t);
-    /* Check user input from form. If valid, create new list item from input */
+    textElement.appendChild(t);
+    /* Check user input from form. If valid, create new list item from input. */
     if (textInput === '') {
-        alert("You need to write a task!");
+        alert('You need to write a task!');
     } else {
-        document.getElementById("tasks").appendChild(task);
-        addDeleteButton(task);
-        addDoneButton(task);
+        document.getElementById('tasks').appendChild(taskClone);
     }
     /* clears textbox after submitting input */
-    document.getElementById("text-box").value = '';
+    document.getElementById('text-box').value = '';
     }
 
 /* delete all tasks on task-list */
 function clearList() {
-    let list = document.getElementById("tasks");
+    let list = document.getElementById('tasks');
     while (list.firstChild) {
         list.removeChild(list.firstChild);
     }
 }
 
-/* create + append Delete Button to each new list item */
-function addDeleteButton(task) {
-    const deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "x";
-    deleteButton.setAttribute("onclick", "deleteTask(this)");
-    task.appendChild(deleteButton);
-} 
+// /* create + append Delete Button to each new list item */
+// function addDeleteButton(task) {
+//     const deleteButton = document.createElement('button');
+//     deleteButton.innerHTML = 'x';
+//     deleteButton.setAttribute('onclick', 'deleteTask(this)');
+//     task.appendChild(deleteButton);
+// } 
 
-/*create + append Done Button to each new list item */
-function addDoneButton(task) {
-    const doneButton = document.createElement("button");
-    doneButton.innerHTML = "Done";
-    doneButton.setAttribute("onclick", "doneTask(this)");
-    task.appendChild(doneButton);
-} 
+// /*create + append Done Button to each new list item */
+// function addDoneButton(task) {
+//     const doneButton = document.createElement('button');
+//     doneButton.innerHTML = 'Done';
+//     doneButton.setAttribute('onclick', 'doneTask(this)');
+//     task.appendChild(doneButton);
+// } 
 
 /* Deletes an individual list item */
 function deleteTask(btn) {
@@ -50,14 +50,14 @@ function deleteTask(btn) {
 
 /* Done Button adds strikethrough styling for list item; changes Done into Restore Button & links to that function in JS */
 function doneTask(btn) {
-    btn.parentNode.style = "text-decoration:line-through; list-style-type:none";
-    btn.innerHTML = "Restore";
-    btn.setAttribute("onclick", "restoreTask(this)");
+    btn.parentNode.style = 'text-decoration:line-through; list-style-type:none';
+    btn.innerHTML = 'Restore';
+    btn.setAttribute('onclick', 'restoreTask(this)');
 }
 
 /* restores list item to normal styling; changes button back to Done */
 function restoreTask(btn) {
-    btn.parentNode.style = "list-style-type:none";
-    btn.innerHTML = "Done";
-    btn.setAttribute("onclick", "doneTask(this)");
+    btn.parentNode.style = 'list-style-type:none';
+    btn.innerHTML = 'Done';
+    btn.setAttribute('onclick', 'doneTask(this)');
 }
